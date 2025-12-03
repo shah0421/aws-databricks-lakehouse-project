3954c2ffcacbbb9f3fb735d4789cbfcae537fa65
Shahriar Mustafa <shahriarcodes8@gmail.com>
2025-12-03 09:42:06 -0500

    update readme.md

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     databricks = {
       source  = "databricks/databricks"
-      version = "~> 1.30.0" # or latest
+      version = "~> 1.30.0"
     }
     aws = {
       source  = "hashicorp/aws"
@@ -13,10 +13,10 @@ terraform {
 
 provider "aws" {
   region  = var.aws_region
-  profile = "terraform_aws" # OR your profile name
+  profile = "terraform_aws"
 }
 
-
+// Fetch Databricks secrets from Secrets Manager
 provider "databricks" {
   host  = local.databricks_secret["databricks_host"]
   token = local.databricks_secret["databricks_token"]
